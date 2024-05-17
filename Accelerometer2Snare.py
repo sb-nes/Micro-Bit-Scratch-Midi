7c6a5b181932f47fcd5d6da7a8753e612fc20fd0
Shreyash Ashm Bara <88775082+sb-nes@users.noreply.github.com>
2024-05-17 23:34:58 +0530

    Update and rename Kick and Snare.py to Accelerometer2Snare.py

---
+++ Accelerometer2Snare.py
@@ -0,0 +1,22 @@
+# this was to test if i can use the accelerometer to trigger a snare
+serial.set_baud_rate(BaudRate.BAUD_RATE115200)
+trig = False
+
+def on_every_interval():
+    global trig
+    serial.write_string("" + String.from_char_code(144) + String.from_char_code(36) + String.from_char_code(90)) # Kick Goes Every Half a Second
+    if input.acceleration(Dimension.X) > 100:
+        serial.write_string("" + String.from_char_code(144) + String.from_char_code(38) + String.from_char_code(90)) 
+        # Snare if Micro:bit is pushed with enough force to the Right -> Sometimes it also causes accidental triggers
+        trig = True
+        
+    sleep(100)
+    serial.write_string("" + String.from_char_code(128) + String.from_char_code(36) + String.from_char_code(90))
+    if (trig):
+        serial.write_string("" + String.from_char_code(128) + String.from_char_code(38) + String.from_char_code(90))
+        trig = False
+loops.every_interval(500, on_every_interval)
+
+def on_forever():
+    pass
+basic.forever(on_forever)
